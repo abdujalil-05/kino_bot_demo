8c05f537081a9f492bbde66f3376b6cea3d5fa4c
abdujalil-05 <dasturchi.bek.05@gmail.com>
2025-09-22 15:47:41 +0500

    Update app.py

--- app.py
+++ app.py
@@ -12,24 +12,47 @@ from middlewares.checkSubscriptionMiddleware import CheckSubscriptionMiddleware
 from handlers.users import users as users_router
 
 
-async def main():
+# async def main():
    
+
+#     dp.include_router(admins_router)
+#     dp.include_router(groups_router)
+#     dp.include_router(channels_router)
+#     dp.include_router(join_router)
+#     core = Core()
+#     moviesDb = MoviesDb()
+#     dp.update.middleware(CheckSubscriptionMiddleware())
+#     dp.include_router(users_router)
+
+#     # Adminlarga xabar berish
+#     await on_startup_notify(bot)
+
+#     # Polling
+#     await dp.start_polling(bot)
+
+
+# if __name__ == "__main__":
+#     asyncio.run(main())
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)  # eski update’larni tozalash
 
     dp.include_router(admins_router)
     dp.include_router(groups_router)
     dp.include_router(channels_router)
     dp.include_router(join_router)
+    dp.include_router(users_router)
+
     core = Core()
     moviesDb = MoviesDb()
+    
     dp.update.middleware(CheckSubscriptionMiddleware())
-    dp.include_router(users_router)
 
     # Adminlarga xabar berish
     await on_startup_notify(bot)
 
     # Polling
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, skip_updates=True)
 
 
-if __name__ == "__main__":
-    asyncio.run(main())
